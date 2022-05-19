6e09c6df55bcfa64da3db11e3d7421c4ff028fb9
deputat1000 <daniilkuchinski@gmail.com>
2022-05-19 12:05:29 +0400

    3 Add the word 'hello' to each element of the array

--- Arrays and Cycles/README.md
+++ Arrays and Cycles/README.md
@@ -16,4 +16,5 @@ node code.js
 ```
 1. Given an array consisting of movie titles, iterate over an array with printing the title of each movie to the console
 2. Given an automaker array, convert the array to a string and back to an array
+3. Given an array of the names of your friends, add the word 'hello' to each element of the array
 ```
--- Arrays and Cycles/code.js
+++ Arrays and Cycles/code.js
@@ -22,3 +22,19 @@ console.log(`\n* To string:\n${automakers}`);
 
 automakers = automakers.split(',').map(item => item = item.trim());
 console.log('* Back to an array:'); console.log(automakers);
+
+//Hello, friends array
+console.log('_____\n3. HELLO, FRIENDS ARRAY');
+
+let friendArr = readlineSync.question('Write the names (comma-separated): ');
+friendArr = friendArr.split(',').map(item => {
+    item = item.trim();
+    item = 'Hello, ' + item;
+
+    return item;
+});
+
+if (friendArr != 'Hello, ') {
+    console.log('\nYour hello to friends:');
+    console.log(friendArr);
+} else console.log('No friends :(');
