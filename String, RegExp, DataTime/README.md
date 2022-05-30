c7213786736501d287e3cd80e5e27f3070a96395
deputat1000 <daniilkuchinski@gmail.com>
2022-05-30 14:20:58 +0400

    2 Write a regular expression that will find a string '2+3'

--- String, RegExp, DataTime/README.md
+++ String, RegExp, DataTime/README.md
@@ -5,4 +5,5 @@ This program executes the following tasks.
 # Tasks
 ```
 1. Given a string 'ahb acb aeb aeeb adcb axeb'. Write a regular expression that will find the strings 'ahb', 'acb', 'aeb' with the pattern: letter 'a', any character, letter 'b'
+2. Given a string '2+3 223 2223'. Write a regular expression that will find a string '2+3' without capturing the rest
 ```
--- String, RegExp, DataTime/code.js
+++ String, RegExp, DataTime/code.js
@@ -10,3 +10,15 @@ let result = str.match(/a.b/g) || 'No matches';
 
 console.log('\n* String:', str, 
             '\n\nSearch result:', result, '| Match counter:', result.length);
+
+//find '2+3'
+{
+console.log(`\n\n2. Find: '2+3'`);
+
+const str = '2+3 223 2223';
+
+result = str.match(/2\+3/g) || 'No matches';
+
+console.log('\n* String:', str,
+            '\n\nSearch result:', result, '| Match counter:', result.length);
+}
